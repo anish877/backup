5745c732e0dcc4a7e439b9ffda86af60450743a7
anish877 <62011810+anish877@users.noreply.github.com>
2025-04-13 12:24:51 +0530

    push

--- src/components/WaterTracker.tsx
+++ src/components/WaterTracker.tsx
@@ -1,6 +1,7 @@
+"use client";
 import React from 'react';
 import { MessageSquare, ArrowRight } from 'lucide-react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const MedicalAIAssistant = () => {
   const router = useRouter()
